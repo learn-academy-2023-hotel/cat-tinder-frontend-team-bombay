5cfa3823131033643b4ed6c8695f4df04275aa03
EWinkeljohn <emcwink@gmail.com>
2023-12-11 13:30:37 -0800

    complete testing card

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -11,8 +11,19 @@ describe("<Header />", () => {
             <Header />
           </BrowserRouter>
         )
+        const headerElement = screen.getByText(/meet/i)
+        expect(headerElement).toBeInTheDocument()
       })
-      const headerElement = screen.getByText(/header/i)
-      expect(headerElement).toBeInTheDocument()
+    it("renders by class name", () => {
+        const { container } = 
+      render(
+        <BrowserRouter>                                                                      
+            <Header />                                                                        
+          </BrowserRouter>
+      )
+      // const container = render(<Header />)
+      const headerComponent = container.querySelector('.header')                                                                                                                                                                     
+      expect(headerComponent).toHaveTextContent('Meet')
+    })
 })
 
--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Home from "../pages/Home"
 import { BrowserRouter } from "react-router-dom"
 
@@ -11,12 +11,13 @@ describe("<Home />", () => {
     expect(homeElement).toBeInTheDocument()
   })
 
-  it("renders an image without crashing", () => {
+  it("renders an image with a src", () => {
     render(
         <BrowserRouter>
             < Home />
         </BrowserRouter>
-
     )
+    const image = screen.getByRole("img")
+    expect(image).toHaveAttribute("src", "https://as2.ftcdn.net/v2/jpg/03/03/62/45/1000_F_303624505_u0bFT1Rnoj8CMUSs8wMCwoKlnWlh5Jiq.jpg")
   })
 })
