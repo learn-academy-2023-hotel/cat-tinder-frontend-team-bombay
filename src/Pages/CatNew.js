ccba0c88713aac57dadf28e55c7b211f7f5b2ccc
kevinrojas <k3vinr07@gmail.com>
2023-12-12 16:17:06 -0800

    create-functionality complete

--- src/Pages/CatNew.js
+++ src/Pages/CatNew.js
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { useNavigate } from 'react-router-dom'
 
-const CatNew = () => {
-    return (
-        <div> CatNew </div> 
 
-    )
-}
+const CatNew = ({ createKitty }) => {
+    
+    const navigate = useNavigate()
 
-export default CatNew
+    const [newKitty, setNewKitty] = useState({
+        name: "",
+        age: "",
+        enjoys: "",
+        image: ""
+    })
+
+    const handleChange = (e) => {
+        setNewKitty({...newKitty, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = () => {
+        createKitty(newKitty)
+        navigate("/catindex")
+    }
+
+
+  return (
+
+    <Form >
+      <FormGroup>
+        <Label for="name" > Name </Label>
+        <Input
+          id="name"
+          name="name"
+          type="text"
+          onChange={handleChange}
+          value={newKitty.name}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Label for="age"> Age </Label>
+        <Input
+          id="age"
+          name="age"
+          type="text"
+          onChange={handleChange}
+          value={newKitty.age}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Label for="enjoys"> Enjoys </Label>
+        <Input
+          id="enjoys"
+          name="enjoys"
+          type="text"
+          onChange={handleChange}
+          value={newKitty.enjoys}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Label for="image"> Image </Label>
+        <Input
+          id="image"
+          name="image"
+          type="text"
+          onChange={handleChange}
+          value={newKitty.image}
+        />
+      </FormGroup>
+
+      <Button onClick={handleSubmit}> Submit </Button>
+    </Form>
+    
+  );
+};
+
+export default CatNew;
