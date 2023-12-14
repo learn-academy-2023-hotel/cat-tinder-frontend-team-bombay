0c3bfc11db84344d8fd36d08acfb724a26da30c7
EWinkeljohn <emcwink@gmail.com>
2023-12-13 16:08:51 -0800

    fetch complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import './App.css'
 import Header from "./components/Header"
@@ -9,27 +9,62 @@ import CatShow from "./pages/CatShow"
 import CatNew from "./pages/CatNew"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
-import mockCats from "./mockCats"
 
 const App = () => {
-  const [cats, setCats] = useState(mockCats)
+  const [cats, setCats] = useState([])
 
-  const createKitty = (kitty) => {
-    console.log('created cat', kitty)
+  useEffect(() => {readCats()}, [])
 
+  const readCats = () => {
+    fetch("http://localhost:3000/cats")
+      .then((response) => response.json())
+      .then((payload) => {setCats(payload)})
+      .catch((error) => console.log(error))
   }
-  const updateCat = (cat, id) => {
-    console.log("cat:", cat)
-    console.log("id", id)
+
+  const createKitty = (createdKitty) => {
+    fetch("http://localhost:3000/cats", {
+      body: JSON.stringify(createdKitty),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then(() => readCats())
+      .catch((error) => console.log("Create error(s): ", error))
+  }
+  const updateCat = (currentCat, id) => {
+    fetch(`http://localhost:3000/cats/${id}`,{
+      body: JSON.stringify(currentCat),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+    })
+    .then((response) => response.json())
+    .then(() => readCats())
+    .catch((error) => console.log("Update error(s): ", error))
   }
 
+  const deleteCat = (id) => {
+    fetch(`http://localhost:3000/cats/${id}`,{
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then(() => readCats())
+    .catch((error) => console.log("Delete error(s): ", error))
+  }
   return (
     <>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/catindex" element={<CatIndex cats={cats} />} />
-      <Route path="/catshow/:id" element={<CatShow cats={cats}/>} />
+      <Route path="/catshow/:id" element={<CatShow cats={cats} deleteCat={deleteCat}/>} />
       <Route path="/catnew" element={<CatNew createKitty={createKitty}/>} />
       <Route path="/catedit/:id" element={<CatEdit cats={cats} updateCat={updateCat}/>} />
       <Route path="*" element={<NotFound />} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+// import CatNew from '../pages/CatNew'
 
 const Header = () => {
+
     return (
         <div className="header">
             <h1>😻 Meet the Paws of your life 😻 </h1>
+            <NavLink to={"/catnew"} className="nav-link">Create Your Pawfile!</NavLink>
         </div>    
     )
 }
